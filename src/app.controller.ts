dfe416e75e6bd78b5499548df4709800bf29d13a
DavidTG7 <davidthekoa@gmail.com>
2022-07-29 14:44:09 -0500

    Separate controllers for responsability

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,27 +18,5 @@ export class AppController {
   @Get('/route/')
   hello() {
     return 'con /sas/';
-  }
-
-  @Get('products/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `Product ${productId}`;
-  }
-
-  @Get('products')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `Products limit=> ${limit} offset=>${offset} brand=> ${brand}`;
-  }
-
-  @Get('categories/:id/products/:productId')
-  getCategories(
-    @Param('productId') productId: string,
-    @Param('id') id: string,
-  ) {
-    return `Product ${productId} and ${id}`;
   }
 }
---
+++ src/controllers/products.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Query, Param } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `Products limit=> ${limit} offset=>${offset} brand=> ${brand}`;
+  }
+
+  @Get('filter')
+  getProductsFilter() {
+    return 'I am a filer!';
+  }
+
+  @Get(':productId')
+  getProduct(@Param('productId') productId: string) {
+    return `Product ${productId}`;
+  }
+}
